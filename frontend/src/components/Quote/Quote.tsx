686b3a1060f4b47104146834cfeda80d66f0504b
Sachin kumar jha <sachinkumarjhasachinkumarjha9@gmail.com>
2025-02-26 20:14:55 +0530

    Some changes

--- frontend/src/components/Quote/Quote.tsx
+++ frontend/src/components/Quote/Quote.tsx
@@ -2,12 +2,12 @@ function Quote() {
   return (
     <div className="bg-[#F8FAFC] h-screen flex  flex-col justify-center p-10">
         <div className="flex justify-center ">
-        <div className="max-w-lg">
-            <div className='text-md lg:text-3xl font-bold'>
+        <div className="max-w-xl">
+            <div className='text-md lg:text-3xl font-bold tracking-wider leading-lg'>
             "The platform offers an intuitive and seamless experience. Creating, publishing, and managing blogs has never been easier."
             </div>
       <div className="max-w-md text-xl font-semibold text-left mt-4">Sachin Jha</div>
-      <div className="max-w-md text-sm font-light text-slate-400">Student | RTU </div>
+      <div className="max-w-md text-sm font-light text-slate-400">Student | RTU ,Kota </div>
         </div>
         </div>
     </div>
