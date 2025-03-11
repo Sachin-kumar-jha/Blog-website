632183a2513fd6b7c62fc2fd0e75f7a96e616f95
Sachin kumar jha <sachinkumarjhasachinkumarjha9@gmail.com>
2025-03-11 23:38:21 +0530

    This is last change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import Signup from "./pages/Signup"
 import Signin from "./pages/Signin"
 import Blogs from './pages/Blogs';
 import Blog from "./pages/Blog"
-import { BrowserRouter,Route,Routes } from "react-router-dom"
 import Publish from "./pages/Publish"
 import MainLayout from "./components/Mainlayout/MainLayout"
 import Profile from "./components/Profile/Profile"
@@ -10,27 +9,26 @@ import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute"
 import Working from "./components/Working/Working"
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
     <div>
-      <BrowserRouter>
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />}/>
-        {/* Protected Routes */}
-        <Route path="/" element={<ProtectedRoute><MainLayout /></ProtectedRoute>}>
-          <Route path="/" element={<Blogs/>}/>
-          <Route path="/blog/:id" element={<Blog />} />
-          <Route path="/publish" element={<Publish />} />
-          <Route path="/user" element={<Profile />}/>
-          <Route path="/user/edit" element={<Working/>}/>
-        </Route>
-      </Routes>
-      </BrowserRouter>
+    <Router>
+  <Routes>
+    <Route path="/signup" element={<Signup />} />
+    <Route path="/signin" element={<Signin />} />
+    <Route path="/" element={<ProtectedRoute><MainLayout /></ProtectedRoute>}>
+      <Route path="/" element={<Blogs />} />
+      <Route path="/blog/:id" element={<Blog />} />
+      <Route path="/publish" element={<Publish />} />
+      <Route path="/user" element={<Profile />} />
+      <Route path="/user/edit" element={<Working />} />
+    </Route>
+  </Routes>
+</Router>
       <ToastContainer position="top-right" autoClose={1000} />
-      </div>
+    </div>
     </>
   )
 }
